7c5eda2e67e3bc9f03d893bd0c7a7a31f2716020
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2025-10-24 16:56:07 +0800

    Initialize Vue 3 app with a greeting message

--- vueandphp/js/app.js
+++ vueandphp/js/app.js
@@ -1,1 +1,10 @@
 console.log(' Hello Vue 3');
+
+const app = Vue.createApp({
+    data (){
+        return{
+            message: 'Hello from Vue 3, Joseph!'
+        }
+    }
+})
+app.mount('#app');
