be4e0ad19eaa9171770d2b0ea31a9a101f7db77a
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2025-10-26 10:18:24 +0800

    Enhance index.php with navigation and profile info
    Added a navigation section with profile information and skills.

--- vueandphp/index.php
+++ vueandphp/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mobile.css">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="js/app.js" defer ></script>
     <title>Joseph Portfolio</title>
@@ -17,6 +18,22 @@
           {{message}}
        </div>
 </header>
+<nav>
+   <div id="banner">
+      <div id="profile-info">
+         <p>{{name}}</p>
+         <code> {{skills}}</code></br>
+         <div id="bio">
+            <p v-bind:style="{fontSize:size,width:width}">{{message}}</p>
+         </div>
+         <button>Hire Me</button>
+      </div>
+      <div id="profile-pic">
+         <img v-bind:src='image' :style="{ width:width ,height:height}" alt="profile-picture" loading="lazy" fetchpriority="high" decoding="async">
+      </div>
+   </div>
+</nav>
 <body>
+   my skills
 </body>
 </html>
