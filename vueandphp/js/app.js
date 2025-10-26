7962d065841d7ab5d9c23ef156912618a0ae9dbe
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2025-10-26 10:18:57 +0800

    Add profile and profile picture components

--- vueandphp/js/app.js
+++ vueandphp/js/app.js
@@ -18,3 +18,27 @@ const app = Vue.createApp({
     }
 })
 app.mount('#app');
+
+const profile=Vue.createApp({
+    data (){
+        return{
+            name:" I'm Joseph Morales",
+            size:"1rem",
+            width:"500px",
+            skills:" Web Developer | Graphic Designer",
+            message:"A passionate Web Developer and Graphic Designer dedicated to crafting visually stunning and user-friendly digital experiences."
+        }
+    }
+})
+profile.mount('#profile-info');
+
+const profilepic=Vue.createApp({
+    data (){
+        return{
+            image:'img/jospeh2025.jpg',
+            width:"400px",
+            height:"400px"
+        }
+    }
+})
+profilepic.mount('#profile-pic');
