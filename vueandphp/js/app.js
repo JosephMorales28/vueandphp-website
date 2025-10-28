f1d35f57c368c8537aff9539c1ce46d014e1c028
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2025-10-28 11:56:08 +0800

    Add about section with developer details
    Added about section with developer information.

--- vueandphp/js/app.js
+++ vueandphp/js/app.js
@@ -3,8 +3,8 @@ console.log(' Hello Vue 3');
 const logo=Vue.createApp({
     data (){
         return{
-            url: '#',
-            title: 'Joseph Morales Portfolio'
+            image:"img/logo.webp",
+            title: 'Joseph Morales'
         }
     }
 })
@@ -42,3 +42,15 @@ const profilepic=Vue.createApp({
     }
 })
 profilepic.mount('#profile-pic');
+
+const about=Vue.createApp({
+    data (){
+        return{
+            title:"About Me",
+            paragraph:"Im a web developer with experience in creating dynamic and responsive websites. I specialize in front-end development using HTML, CSS, Vue.js, and Javascript.",
+            paragraphtwo:"I have a strong understanding of user experience and design principles, and I am passionate about creating visually appealing and user-friendly websites.",
+            paragraphthree:"I am also familiar with back-end development using PHP and MySQL, allowing me to create full stack web applications. I am constantly learning and staying up to date with the latest web development trends and technologies to ensure that my skills are always current."
+        }
+    }
+})
+about.mount('#Aboutme');
