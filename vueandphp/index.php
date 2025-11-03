35309e2d0c7a7dd85e13c532cf00b7b950f7d915
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2025-11-03 09:53:38 +0800

    Refactor index.php for improved structure and content
    Updated the structure and content of the HTML document, including changes to the header, navigation, and body sections.

--- vueandphp/index.php
+++ vueandphp/index.php
@@ -21,93 +21,65 @@
     <script src="js/app.js" defer ></script>
     <title>Joseph Portfolio</title>
 </head>
-<header oncontextmenu="return false;">
-       <div id="logo">
-          <picture>
-             <source v-bind:srcset="image" type="image/webp">
-                <img v-bind:src="image" alt="logo" loading="lazy" fetchpriority="high" decoding="async">
-          </picture>
-          <h1>{{title}}</h1>
-       </div> 
-       <div id="app">
-          <div id="blog"><p>{{blog}}</p></div>
-          <button>{{message}}</button>
-       </div>
-</header>
-<nav oncontextmenu="return false;">
-   <div id="banner">
-      <div id="profile-info">
-         <p>{{name}}</p>
-         <code> {{skills}}</code></br>
-         <div id="bio">
-            <p v-bind:style="{fontSize:size,width:width}">{{message}}</p>
-         </div>
-         <button>Hire Me</button>
-      </div>
-      <div id="profile-pic">
+<body oncontextmenu= "return false;">
+   <header>
+      <div id="logo">
          <picture>
-            <source v-bind:srcset='image' type="image/webp">
-            <img v-bind:src='image' :style="{ width:width ,height:height}" alt="profile-picture" loading="lazy" fetchpriority="high" decoding="async">
+            <source srcset="img/logo.webp" type="image/webp">
+               <img v-bind:src="image" alt="logo" loading="lazy" fetchpriority="high" decoding="async">
          </picture>
-      </div>
-   </div>
-</nav>
-<body oncontextmenu="return false;" ondragstart="return false;" onselectstart="return false;">
-   <div id="info">
-      <div id="Aboutme">
          <h1>{{title}}</h1>
-         <p>{{paragraph}}</br></br>{{paragraphtwo}}</br></br>{{paragraphthree}}</p>
       </div>
-      <div id="clientsAverage" oncontextmenu="return false;">
-         <img src="img/increase calendar copy.png" alt="clients-icon" loading ="lazy" fetchpriority="high" decoding="async">
-         <h1>50+</h1>
-         <p>Average increase in clients engagement in the first 6 months.</p></br></br>
-         <div id="profile-pic-small">
-            <img src="img/josephoct.webp" alt="profile-picture" loading="lazy" fetchpriority="high" decoding="async">
+      <div id="right-header">
+          <h1><a href="blog.php">{{message}}</a></h1>
+          <button>{{button}}</button>
+      </div>
+   </header>
+   <nav>
+      <div id="profile">
+         <div id="profile-info">
+            <h1>{{name}}</h1>
+            <code>{{skills}}</code>
+            <p>{{message}}</p>
+            <button>Hire me</button>
+         </div>
+         <img v-bind:src="image" alt="profile picture" loading="lazy" fetchpriority="high" decoding="async">
+      </div>
+   </nav>
+   <section>
+      <div id="About-me">
+         <div id="about-text">
+            <h1>{{title}}</h1>
+            <p>{{paragraph}}</p>
+            <p>{{paragraphtwo}}</p>
+            <p>{{paragraphthree}}</p>
+         </div>
+         <div id="about-content">
+            <img src="img/increase calendar copy.png" alt="increase" loading="lazy" fetchpriority="high" decoding="async">
+            <h1>50+</h1>
+            <p>Project Completed in the past 6 months</p>
+            <div id="content-image">
+               <img src="img/josephoct.webp" alt="joseph" loading="lazy" fetchpriority="high" decoding="async">
+            </div>
+         </div>
+         <div id="about-education">
+            <img src="img/josephoct.webp" alt="joseph" oading="lazy" fetchpriority="high" decoding="async">
+            <h1>Educationional Background</h1>
+            <uL>
+               <li>Holy child Catholic School - Primary</li>
+               <li>University of the East Manila - Secondary</li>
+               <li>University of the East Manila (CCSS) - Tertiary</li>
+               <li>TESDA - Training</li>
+               <li>MFI - Training</li>
+            </ul>
          </div>
       </div>
-      <div id="experience" oncontextmenu="return false;">
-         <picture>
-            <source v-bind:srcset="image" type="image/webp">
-               <img v-bind:src="image" alt="profile-picture" loading="lazy" fetchpriority="high" decoding="async">
-            </source>
-         <picture>
-         </br></br>
-         <h1>{{h1}}</h1>
-         <?php
-              $Educational=array("Holy Child Catholic School-Primary", "University of the East Manila- Secondary", "University of the East Manila (CCSS)- Tertiary", "Tesda", "MFI");
-
-              foreach ($Educational as $educ){
-                 print "<ul>";
-                 print "<li> $educ </li>";
-                 print "</ul>";
-              }
-         ?>
-      </div>
-   </div>
-   <div id="portfoliopage">
-        <div id="portfolioheader">
-           <h1>Projects</h1>
-           <p>Here are some of my recent works</p>
-        </div>
-        <div id="portfoliolist">
-            <ul>
-               <li><div id="portfolioitem"><img src="img/sample.jpg" alt="mountains" loading="lazy" fetchpriority="high" decoding="async"><p>description:</br></br>mountains</p><p>programming language use </br></br><code>SCSS</code> <code>Vue</code> <code>php</code></p></div></li>
-               <li><div id="portfolioitem"><img src="img/sample.jpg" alt="mountains" loading="lazy" fetchpriority="high" decoding="async"><p>description:</br></br>mountains Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quod eligendi atque sed dolores, tenetur dolorem mollitia blanditiis reiciendis velit iure excepturi at fugiat optio qui assumenda, iusto eveniet reprehenderit?</p><p>programming language use </br></br><code>SCSS</code> <code>Vue</code> <code>php</code></p></div></li>
-               <li><div id="portfolioitem"><img src="img/sample.jpg" alt="mountains" loading="lazy" fetchpriority="high" decoding="async"><p>description:</br></br>mountains</p><p>programming language use </br></br><code>SCSS</code> <code>Vue</code> <code>php</code></p></div></li>
-            </ul>
-        </div>
-   </div>
+   </section>
 </body>
-<section id="sectionone">
-<h1>{{heading}}</h1>
-<p>{{paragraph}}</p>            
-</section>
 <footer>
    <p>&copy; {{paragraph}}</p>
    <ul>
-      <li><img v-for="(socialmedia, index) in image" :key=index v-bind:src="socialmedia" :alt="list[index]" loading="lazy" fetchpriority="high" decoding="async"></li>
+      <li><img v-for="(socialmedia, index) in image" :key="index" v-bind:src="socialmedia" :alt="list[index]" loading="lazy" fetchpriority="high" decoding="async"></li>
    </ul>
 </footer>
 </html>
-
